a9a8a5171486cf5dd13aa5d080cdcbcceabfd5a7
jsdkfj@jklsd.com <jsdjfkls@jklfdg.com>
2022-02-07 19:48:10 +0600

    ss

--- commonfun/errorHandeling.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require("express-validator");
-const addUserValidationHandler = function (req, res, next) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-    next();
-  };
-module.exports = addUserValidationHandler;
